a6e7c050202fd0a25a794ac14a5c14135ab5a4db
gulerkotan <gulerkotan97@gmail.com>
2019-07-19 16:44:31 +0300

    ekleme

--- app/User.php
+++ app/User.php
@@ -2,6 +2,7 @@
 
 namespace App;
 
+use App\Models\Api\KullaniciHakkinda;
 use Illuminate\Notifications\Notifiable;
 use Illuminate\Contracts\Auth\MustVerifyEmail;
 use Illuminate\Foundation\Auth\User as Authenticatable;
@@ -36,4 +37,8 @@ class User extends Authenticatable
     protected $casts = [
         'email_verified_at' => 'datetime',
     ];
+
+    public function Hakkinda(){
+        return $this->hasOne(KullaniciHakkinda::class);
+    }
 }
